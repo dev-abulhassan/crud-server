9f4ee1788c1bb084d353b8b88e5489d1b17da586
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-09 02:23:45 +0600

    crud server

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middeleware config
+app.use(cors());
+app.use(express.json());
+
+
+// adnanhassaninfo
+// 7pWfr85LvWRNDh9x
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://adnanhassaninfo:<password>@cluster0.eqk9iwm.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+   res.send('Simple crud in runnint')
+})
+app.listen(port), () => {
+   console.log('listening on port, $(port)');
+};
